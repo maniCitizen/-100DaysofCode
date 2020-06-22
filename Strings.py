1525bb2630ba56d07e2af9e66674a776f9dcf211
tamila <tamilaguru08150@gmail.com>
2020-06-22 15:42:48 +0530

    Learned Strings(Basics)

---
+++ Strings.py
@@ -0,0 +1,6 @@
+print("Tamil's laptop")
+print('Tamil "laptop" ')
+print('Tamil\'s "Laptop"')  #Used Escape characters
+print("tamil"+ " " +"tamil")
+print(r'c:\docs\navin')  #Used raw string to disable special meaning of \n.
+print(r'c:\docs\tamil')  # Again used raw string to disable \t.
