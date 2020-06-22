4b0c9da11abd1e91616330856b73338c5f333f2c
tamila <tamilaguru08150@gmail.com>
2020-06-22 15:11:51 +0530

    Made my first Commit Python As A Calculator

---
+++ Python As A Calculator.py
@@ -0,0 +1,10 @@
+print(2+3)  #addition
+print(9-8)  #subtraction
+print(4*6)  #multiplication
+print(8/4)  #float division
+print(8//4) #floor division
+print(5/2)  #float division
+print(5//2)  #floor division
+print(2**3)  #exponentiation
+print((8+2)*3) # Use of Brackets
+print(3%2)  #Getting remainder only using Modulus
