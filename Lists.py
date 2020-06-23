027dd0bc503d17eafdf900ec3faf7339d809264a
tamila <tamilaguru08150@gmail.com>
2020-06-23 23:08:16 +0530

    Learned Lists

---
+++ Lists.py
@@ -0,0 +1,23 @@
+print(range(0, 10))
+even = list(range(0, 10, 2))
+odd = list(range(1, 10, 2))
+print(even)
+print(odd)
+
+my_string = "abcdefghijklmnopqrstuvwxyz"
+print(my_string.index("e"))
+print(my_string[4])
+
+small_decimals = list(range(5, 10))
+print(small_decimals)
+print(small_decimals.index(8))
+
+odd = range(0, 1000000, 2)
+
+print(odd[985])
+print(odd.index(1970))
+
+small_decimals = (range(0, 10))
+my_range = small_decimals[::2]
+print(my_range)
+print(my_range.index(4))
