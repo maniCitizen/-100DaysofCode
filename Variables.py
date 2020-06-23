253e12e3cf28762a7a07f52b372c372180200d23
tamila <tamilaguru08150@gmail.com>
2020-06-23 15:22:58 +0530

    Strings Completed

---
+++ Variables.py
@@ -0,0 +1,20 @@
+x = 2
+y = 3
+print(x+2)
+print(x)
+#Note: If any variable is not assigned it gives Nameerror
+# If you want the output of previous operation then we can use _
+name = "Youtube"
+print(name + " rocks")
+print(name[0])  #Fetching individual characters.
+print(name[-1])  # Negative indexing
+print(name[0:2])
+print(name[1:])
+print(name[:7])
+print(name[:])
+print(name[::-1])
+print(name[3:10])
+ #name[0:3] = "my"  #Strings are immutable.
+#To find the length of the function we use len()
+l = len(name)
+print(l)
