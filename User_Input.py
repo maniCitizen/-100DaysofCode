395879e3488c4bf80b387863eb836cf95ec44308
tamila <tamilaguru08150@gmail.com>
2020-07-05 01:04:39 +0530

    Learned User Input

---
+++ User_Input.py
@@ -0,0 +1,19 @@
+x = int(input("Please enter an integer : "))
+y = int(input("Please enter another integer : "))
+z = x + y
+print(z)
+
+#Fetching the char from the input
+x = input("Enter a character : ")[0]
+print(x)
+
+#Using Eval function
+result = eval(input("Please enter an expression : "))
+print(result)
+
+#Accepting input from Command line
+import sys
+x = sys.argv[1]
+y = sys.argv[2]
+print(x+y)
+
