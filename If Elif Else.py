3f25f2302cc63afb3448bf052011d35d72ac3dd0
tamila <tamilaguru08150@gmail.com>
2020-07-05 21:04:05 +0530

    Changed a little code

--- If Elif Else.py
+++ If Elif Else.py
@@ -1,14 +1,14 @@
-# x = 8
-# r = x % 2
+x = 8
+r = x % 2
 
-# if r==0:
-#     print("Even")
+if r==0:
+    print("Even")
     
-#     if x > 5:
-#         print("Great")
+    if x > 5:
+        print("Great")
 
-# else:
-#     print("Odd")
+else:
+    print("Odd")
 
 x = int(input("Please enter a number : "))
 
