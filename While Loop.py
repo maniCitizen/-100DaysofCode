b46aef6acbbd4743b2949549e8bcc00d48c745d5
tamila <tamilaguru08150@gmail.com>
2020-07-06 00:13:52 +0530

    Learned While Loop

---
+++ While Loop.py
@@ -0,0 +1,14 @@
+i = 1
+
+
+while i <= 5:
+    print("Tamil ", end = "")
+    
+    j = 1
+    while j <= 4:
+        print("Rocks ", end = "")
+        j = j+1
+        
+    i = i+1
+    print("\n")
+    
