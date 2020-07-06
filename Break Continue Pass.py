ebe4126722947c4cfb991acf226ee743dae79490
tamila <tamilaguru08150@gmail.com>
2020-07-06 16:10:32 +0530

    Learned Break Continue Pass

---
+++ Break Continue Pass.py
@@ -0,0 +1,32 @@
+av = 5
+x = int(input("How many candies do you want ?"))
+i = 1
+
+while i <= x:
+    
+    if i > av:
+        print("Out of Stock")
+        break
+    
+    print("Candy")
+    i += 1
+    
+for i in range(1,101):
+    
+    if i%3 == 0 or i%5 ==0:
+        continue
+    
+    print(i)
+    
+print("Bye")
+
+for i in range(1, 101):
+    
+    if i%2!=0:
+        pass
+    
+    else:
+        print(i)
+
+print("Bye")
+
