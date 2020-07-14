a3b4ebcfb5d357bb1cdbe1fcdf923a7acb267e1f
tamila <tamilaguru08150@gmail.com>
2020-07-14 09:47:49 +0530

    Learned Printing patterns

---
+++ Printing Patterns.py
@@ -0,0 +1,12 @@
+for i in range(1, 5):
+    for j in range(i):
+        print("# ", end = "")
+        
+    print("\n")
+    
+for i in range(4):
+    for j in range(4-i):
+        print("# ", end = "")
+        
+    print("\n")  
+
