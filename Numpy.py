04ed3a0aa0c3c6d25971d909638168c8e0512dd6
tamila <tamilaguru08150@gmail.com>
2020-07-15 11:29:57 +0530

    Ways of creating arrays in Numpy

---
+++ Numpy.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+arr = np.array([1,2,3,4,5.0], float)
+
+arr = np.linspace(1,20)
+
+arr = np.arange(1,15,2)
+
+arr = np.logspace(1,10,5)
+
+arr = np.ones(5, int)
+
+arr = np.zeros(5, int)
+
+print(arr.dtype)
+
+print(arr)
