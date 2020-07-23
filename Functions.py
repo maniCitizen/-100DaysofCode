eeea12ca7be47ed87faefa3734783d563a11b966
tamila <tamilaguru08150@gmail.com>
2020-07-23 12:30:47 +0530

    Basics of Functions

---
+++ Functions.py
@@ -0,0 +1,15 @@
+def greet():
+    print("Hello")
+    print("Good Morning")
+    
+# greet()
+
+# greet()
+
+def add_sub(x, y):
+    c = x+y
+    d = x-y
+    return c,d
+    
+result1, result2 = add_sub(4,6)
+print(result1, result2)
