e72670d77e30660d116867fa5612bd67c73d91b2
tamila <tamilaguru08150@gmail.com>
2020-07-26 00:25:28 +0530

    Types of Arguments

---
+++ Types Of Arguments.py
@@ -0,0 +1,43 @@
+def greet(name, msg):
+    """This function greets to
+    the person with the provided message"""
+    print("Hello", name + ', ' + msg)
+
+greet("Monica", "Good morning!")
+
+def greet(name, msg="Good morning!"):
+    """
+    This function greets to
+    the person with the
+    provided message.
+
+    If the message is not provided,
+    it defaults to "Good
+    morning!"
+    """
+
+    print("Hello", name + ', ' + msg)
+
+
+greet("Kate")
+greet("Bruce", "How do you do?")
+
+# 2 keyword arguments
+greet(name = "Bruce",msg = "How do you do?")
+
+# 2 keyword arguments (out of order)
+greet(msg = "How do you do?",name = "Bruce") 
+
+1 positional, 1 keyword argument
+greet("Bruce", msg = "How do you do?")  
+
+def greet(*names):
+    """This function greets all
+    the person in the names tuple."""
+
+    # names is a tuple with arguments
+    for name in names:
+        print("Hello", name)
+
+
+greet("Monica", "Luke", "Steve", "John")
